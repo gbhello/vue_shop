c436779350d133ac8dc34f85adbc4db75e718518
gengbin <gbhello@foxmail.com>
2020-04-06 00:19:15 +0800

    登录组件头部布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '/login',
+    component: Login
+  },
+  // 根路径重定向到登录页
+  {
+    path: '/',
+    redirect: '/login'
+  }
 ]
 
 const router = new VueRouter({
