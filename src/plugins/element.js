fb04b0dedfd29da8b2db2c5a675a77633205b651
gengbin <gbhello@foxmail.com>
2020-04-06 22:33:29 +0800

    优化element-ui按需导入

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,5 @@
-/* eslint-disable import/no-duplicates */
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import { Form, FormItem } from 'element-ui'
-import { Input } from 'element-ui'
-
-// 导入弹框提示组件
-import { Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
