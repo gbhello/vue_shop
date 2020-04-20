5c5952ca0b0e64620af1e51e5fa8e24b8179aa6f
gengbin <gbhello@foxmail.com>
2020-04-21 00:19:02 +0800

    稳定运行版本

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ import './assets/css/global.css'
 
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入nprogress
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import axios from 'axios'
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
@@ -20,9 +24,15 @@ import 'quill/dist/quill.bubble.css'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
-// 配置请求头
+// 配置请求头,同时在request拦截器中，展示进度条NProgress进度条
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 在response拦截器中，隐藏进度条
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
