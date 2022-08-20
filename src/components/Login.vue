5ca883ba580be54585496f2620e38ba6e0032204
glh <2654494670@qq.com>
2022-08-20 18:25:04 +0800

    登录表单重置

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,7 +7,7 @@
                 <img src="../assets/logo.png" alt="">
             </div>
             <!-- 登陆表单区域 -->
-            <el-form :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+            <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
                 <!-- 用户名 -->
                 <el-form-item prop="username">
                     <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
@@ -19,7 +19,7 @@
                 <!-- 按钮区域 -->
                 <el-form-item class="btns">
                     <el-button type="primary">登录</el-button>
-                    <el-button type="info">重置</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -49,6 +49,13 @@ export default {
                     { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
                 ]
             }
+        }
+    },
+    methods:{
+        // 点击重置按钮，重置登陆表单
+        resetLoginForm(){
+            // console.log(this);
+            this.$refs.loginFormRef.resetFields();
         }
     }
 }
