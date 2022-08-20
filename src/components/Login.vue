301a38e9f9424da2a350e06568ed96a2b57aadd3
glh <2654494670@qq.com>
2022-08-20 16:45:29 +0800

    登陆组件布局

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,7 @@
 //模板
 <template>
-    <div>
-        登陆组件
+    <div class="login_container">
+        <div class="login_box"></div>
     </div>
 </template>
 
@@ -12,4 +12,18 @@ export default {}
 
 //样式
 <style lang="less" scoped>
+.login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
 </style>
