af1be1e92013fb39e2bb0515241a2c08335be22f
glh <2654494670@qq.com>
2022-08-21 04:25:26 +0800

    左侧菜单美化完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,26 +13,24 @@
     <!-- 侧边栏 -->
     <el-aside width="200px">
         <!-- 侧边栏菜单区域 -->
-        <el-menu
-      background-color="#333744"
-      text-color="#fff"
-      active-text-color="#ffd04b">
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff" unique-opened>
       <!-- 一级菜单 -->
-      <el-submenu index="1">
+      <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id">
         <!-- 一级菜单的模板区域 -->
         <template slot="title">
           <!-- 图标 -->
-          <i class="el-icon-location"></i>
+          <i :class="iconsObj[item.id]"></i>
           <!-- 文本 -->
-          <span>导航一</span>
+          <span>{{item.authName}}</span>
         </template>
+
         <!-- 二级菜单 -->
-        <el-menu-item index="1-4-1">
+        <el-menu-item :index="subItem.id+''" v-for="subItem in item.children" :key="subItem.id">
             <template slot="title">
           <!-- 图标 -->
-          <i class="el-icon-location"></i>
+          <i class="el-icon-menu"></i>
           <!-- 文本 -->
-          <span>导航一</span>
+          <span>{{subItem.authName}}</span>
         </template>
         </el-menu-item>
       </el-submenu>
@@ -50,7 +48,14 @@ export default {
     data(){
         return{
             // 左侧菜单数据
-            menulist:[]
+            menulist:[],
+            iconsObj:{
+                '125':'iconfont icon-user',
+                '103':'iconfont icon-tijikongjian',
+                '101':'iconfont icon-shangpin',
+                '102':'iconfont icon-danju',
+                '145':'iconfont icon-baobiao'
+            }
         }
     },
     created(){
@@ -95,8 +100,14 @@ export default {
 }
 .el-aside{
     background-color: #333744;
+    .el-menu{
+        border-right: none;
+    }
 }
 .el-main{
     background-color: #eaedf1;
+}
+.iconfont{
+    margin-right: 10px;
 }
 </style>
