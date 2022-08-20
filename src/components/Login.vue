dd9ce616727c92e0f43625a1fed933006a456624
glh <2654494670@qq.com>
2022-08-20 19:34:31 +0800

    登陆成功后的行为

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -33,8 +33,8 @@ export default {
         return{
             // 这是登陆表单的数据绑定对象
             loginForm:{
-                username:'zs',
-                password:'123'
+                username:'admin',
+                password:'123456'
             },
             // 这是表单的验证规则对象
             loginFormRules:{
@@ -64,7 +64,14 @@ export default {
                 const {data:res}= await this.$http.post("login",this.loginForm);
                 // console.log(res);
                 if(res.meta.status !== 200) return this.$message.error("登陆失败");
-                this.$message.success("登陆成功")
+                this.$message.success("登陆成功");
+                // 1.将登陆成功之后的 token ，保存到客户端的 sessionStorage 中
+                //   1.1 项目中除了登陆之外的其他API接口，必须在登录之后才能访问
+                //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+                // console.log(res);
+                window.sessionStorage.setItem('token',res.data.token);
+                // 2.通过编程式导航($router)跳转到后台主页，路由地址是 /home
+                this.$router.push("/home");
             })
         }
     }
