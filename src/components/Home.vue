2cdc2c73126f50a3586dc5e6de650cb77f0fae27
glh <2654494670@qq.com>
2022-08-21 02:54:24 +0800

    主页header布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,13 @@
 <template>
 <el-container class="home-container">
     <!-- 头部区域 -->
-  <el-header>Header <el-button type="info" @click="logout">退出</el-button></el-header>
+  <el-header>
+    <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+    </div>
+    <el-button type="info" @click="logout">退出</el-button>
+   </el-header>
   <!-- 页面主体区域 -->
   <el-container>
     <!-- 侧边栏 -->
@@ -30,6 +36,19 @@ export default {
 }
 .el-header{
     background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+        display: flex;
+        align-items: center;
+        span{
+            margin-left: 15px;
+        }
+    }
 }
 .el-aside{
     background-color: #333744;
